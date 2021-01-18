78d0d3a56f017681cea82727726c0bab5979589b
mkreitler <mkreitler@hotmail.com>
2021-01-18 12:32:07 -0700

    First commit.

---
+++ Assets/Scripts/PlayerLadder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assertions;
+
+namespace PalletBuilder {
+    public class PlayerLadder : MonoBehaviour {
+        [SerializeField]
+        private CharacterController charController = null;
+
+        // Start is called before the first frame update
+        void Start() {
+            Assert.Test(charController != null, "No character controller found!");
+        }
+
+        private void FixedUpdate() {
+            if (charController != null) {
+                if (OVRInput.Get(OVRInput.Button.Three) || OVRInput.Get(OVRInput.Button.Four)) {
+                    charController.height = 3.0f;
+                }
+                else {
+                    charController.height = 1.0f;
+                }
+            }
+        }
+    }
+}
